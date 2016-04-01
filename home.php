9ea2277d268572dd5b9a62665f693a939a9439f1
IanMezza <ian.mezza@gmail.com>
2016-04-01 10:45:03 -0600

    Not an initial commit

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+/* home.php */
+$context = array();
+
+/**
+ * Fecha
+ */
+$context['date'] = date_i18n('j \d\e F \d\e Y', time());
+/**
+ * Popular dishes
+ */
+$context['popularDishes'] = Timber::get_posts('cat=2&numberposts=4');
+/**
+ * Laest dishes
+ */
+$context['latestDishes'] = Timber::get_posts('cat=3&numberposts=3');
+/**
+ * Renderea la plantilla
+ */
+Timber::render('home.twig', $context);
