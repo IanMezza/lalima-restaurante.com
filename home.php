9737b0b61ff26ef18c5687e81159500941d6796d
IanMezza <ian.mezza@gmail.com>
2016-04-26 17:57:07 -0500

    Promos added

--- home.php
+++ home.php
@@ -7,6 +7,10 @@ $context = array();
  */
 $context['date'] = date_i18n('j \d\e F \d\e Y', time());
 /**
+ * Next/Recent events
+ */
+$context['promoEvents'] = Timber::get_posts('cat=4&numberposts=2');
+/**
  * Popular dishes
  */
 $context['popularDishes'] = Timber::get_posts('cat=2&numberposts=4');
